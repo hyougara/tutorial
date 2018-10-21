644b4de8f43a327dfee7ae27fc27c3be6fe67c57
前田優樹 <orzazous2@gmail.com>
2018-10-21 12:57:40 +0900

    user登録form 画像はまだ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+   #  @user = User.new(params[:user])[:user]はシンボル
+      # @user = User.new(name: "Foo Bar", email: "foo@invalid",
+      # password: "foo", password_confirmation: "bar")と同じ事
+      # [:user]というシンボルにUser.newで入力したデータを代入している
+      
+      @user = User.new(user_params) # user_paramsメソッドはUsersコントローラの内部でのみ実行,privateで実行
+    if 
+      @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+# privateキーワードでキーワードで外部から不正に使われないようにする
+  private 
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
